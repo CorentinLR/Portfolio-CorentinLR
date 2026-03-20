5d0458aa429b60b791f7e9286a788eea14395cec
Corentinho <corentinlr10@gmail.com>
2026-03-20 01:22:41 +0100

    Updates

---
+++ projects/pokemon/collection.js
@@ -0,0 +1,78 @@
+let collectionData = [
+    {
+        collection_id: 1,
+        pokemon_id: 2,
+        pokemon_nickname: "Plante Bizarre"
+    },
+    {
+        collection_id: 2,
+        pokemon_id: 2,
+        pokemon_nickname: "Patate"
+    },
+    {
+        collection_id: 12,
+        pokemon_id: 143,
+        pokemon_nickname: "Ronflex"
+    },
+    {
+        collection_id: 13,
+        pokemon_id: 10,
+        pokemon_nickname: "Une Chenille"
+    },
+    {
+        collection_id: 4,
+        pokemon_id: 50,
+        pokemon_nickname: "Monstre"
+    },
+    
+    {
+        collection_id: 5,
+        pokemon_id: 51,
+        pokemon_nickname: "Triple Monstre"
+    },
+    {
+        collection_id: 6,
+        pokemon_id: 128,
+        pokemon_nickname: "Poumba"
+    },
+    {
+        collection_id: 7,
+        pokemon_id: 25,
+        pokemon_nickname: "Pikachu"
+    },
+    {
+        collection_id: 8,
+        pokemon_id: 195,
+        pokemon_nickname: "Maraistre"
+    },
+    {
+        collection_id: 9,
+        pokemon_id: 124,
+        pokemon_nickname: "Alban"
+    },
+
+
+    {
+        collection_id: 10,
+        pokemon_id: 101,
+        pokemon_nickname: "Voltorbe"
+    },
+    {
+        collection_id: 11,
+        pokemon_id: 100,
+        pokemon_nickname: "Electrode"
+    },
+    {
+        collection_id: 3,
+        pokemon_id: 45,
+        pokemon_nickname: "Champi"
+    },
+    {
+        collection_id: 5433,
+        pokemon_id: 2,
+        pokemon_nickname: "Pamplemousse"
+    },   
+
+]
+
+
