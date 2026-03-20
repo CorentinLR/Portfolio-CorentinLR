34e2d89e6139289cca556d2738eaf807e2e44266
Corentinho <corentinlr10@gmail.com>
2026-03-20 01:25:14 +0100

    Fix: Add basename for GitHub Pages deployment

--- site-react/src/App.jsx
+++ site-react/src/App.jsx
@@ -13,7 +13,7 @@ function ScrollComponent() {
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
