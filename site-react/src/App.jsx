4ce08a8d5d3e27f0f71e432b00d389f35cbb5a91
Corentinho <corentinlr10@gmail.com>
2026-03-20 01:29:40 +0100

    Fix: Switch to HashRouter for reliable GitHub Pages deployment

--- site-react/src/App.jsx
+++ site-react/src/App.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import AllProjects from './components/AllProjects';
 import ScrollToTop from './components/ScrollToTop';
 
 // Helper to scroll top on navigation
 function ScrollComponent() {
-  // We can create a simple ScrollToTop component usually, 
-  // but for now let's just make sure we export a clean router.
   return null;
 }
 
 function App() {
-  const basename = import.meta.env.BASE_URL.replace(/\/$/, '');
-  
   return (
-    <Router basename={basename}>
+    <Router>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
